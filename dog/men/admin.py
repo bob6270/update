b8b504dbb004f00d746a2991ec6f143c4d72a037
bob6270 <70320694+bob6270@users.noreply.github.com>
2020-09-18 15:59:41 -0700

    Add files via upload

---
+++ dog/men/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Display
+from .models import Display2
+from .models import Display3
+# Register your models here.
+admin.site.register (Display)
+admin.site.register (Display2)
+admin.site.register (Display3)
